d8c608789b3b23cded84f05ecaf09857f11634c7
delaluz12 <75863526+delaluz12@users.noreply.github.com>
2021-07-28 18:20:04 -0500

    saving current local branch changes

---
+++ assets/script.js
@@ -0,0 +1,25 @@
+jQuery(function( $ ) {
+    // Your code using failsafe $ alias here...
+
+    //Type Category of the activity
+    //put into drop down menu
+    // ["education", "recreational", "social", "diy", "charity", "cooking", "relaxation", "music", "busywork"]
+    var categoryUrl = 'https://www.boredapi.com/api/activity?type=recreational'
+    fetch(categoryUrl)
+        .then(function (response) {
+            if (response.ok) {
+                response.json().then(function (data) {
+                    console.log('Search by Type of Activity-----');
+                    console.log(data)
+                    console.log(data.activity)
+                    //pass activity to search yt videos
+                    // searchVideos(data.activity);
+                })
+            } else {
+                alert('Error: ' + response.statusText);
+            }
+        })
+        .catch(function (error) {
+            alert('Error connecting to API server');
+        });
+  });
