8448827cd0105b4dab9d9417ecb664f74372e72d
delaluz12 <75863526+delaluz12@users.noreply.github.com>
2021-07-28 20:00:25 -0500

    finished categoryAPI call for dropdown

--- assets/script.js
+++ assets/script.js
@@ -1,27 +1,62 @@
-jQuery(function( $ ) {
+jQuery(function ($) {
     // Your code using failsafe $ alias here...
 
-    //Type Category of the activity
-    //put into drop down menu
-    // ["education", "recreational", "social", "diy", "charity", "cooking", "relaxation", "music", "busywork"]
-    var categoryUrl = 'https://www.boredapi.com/api/activity?type=recreational'
-    fetch(categoryUrl)
-        .then(function (response) {
-            if (response.ok) {
-                response.json().then(function (data) {
-                    console.log('Search by Type of Activity-----');
-                    console.log(data)
-                    console.log(data.activity)
-                    //pass activity to search yt videos
-                    // searchVideos(data.activity);
-                })
-            } else {
-                alert('Error: ' + response.statusText);
-            }
-        })
-        .catch(function (error) {
-            alert('Error connecting to API server');
-        });
 
-        console.log($('option'));
-  });
+    //capture value of selected category from dropdown
+    $('#categorySearch').on('click', function () {
+        // console.log($('.browser-default').val());
+        var categoryClicked = $('.browser-default').val();
+        //pass value to make api call
+        categoryCall(categoryClicked);
+    })
+
+    var categoryCall = function (category) {
+
+        //make call to gather category endpoint data
+        
+        var categoryUrl = 'https://www.boredapi.com/api/activity?type=' + category
+        fetch(categoryUrl)
+            .then(function (response) {
+                if (response.ok) {
+                    response.json().then(function (data) {
+                        console.log('Search by Type of Activity-----');
+                        console.log(data)
+                        // console.log(data.activity);
+                        var catActivity = data.activity;
+                        console.log(catActivity);
+                        // console.log(((data.accessibility)*100) + '%')
+                        var catAccess = ((data.accessibility)*100) + '%';
+                        console.log(catAccess);
+                        // console.log(data.type)
+                        var catCategory= data.type;
+                        console.log(catCategory);
+                        // console.log(data.participants)
+                        var catParticipants = data.participants;
+                        console.log(catParticipants);
+                        // console.log(data.price)
+                        var catPrice = data.price;
+                        console.log(catPrice);
+                        //pass to displayResults function ====> displayResults (catActivity, catAccess, catCategory, catParticipants, catPrice)
+                        //pass activity to search yt videos ====> searchVideos(catActivity);
+                    })
+                } else {
+                    alert('Error: ' + response.statusText);
+                }
+            })
+            .catch(function (error) {
+                alert('Error connecting to API server');
+            });
+
+    }
+
+
+
+    $('select').formSelect();
+
+
+
+
+
+
+
+});
