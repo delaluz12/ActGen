b97dda527434f60e8b811fa7d835d161a43f04db
blacxcellence <tgtakawira@gmail.com>
2021-07-30 12:00:02 -0500

    added Save funcion based on list item text(may be updated based on how text is rendered)

--- assets/script.js
+++ assets/script.js
@@ -1,103 +1,8 @@
-$(document).ready(function () {
-
-    var categoryCall = function (category) {
-        //make call to gather category endpoint data
-        var categoryUrl = 'https://www.boredapi.com/api/activity?type=' + category
-        fetch(categoryUrl)
-            .then(function (response) {
-                if (response.ok) {
-                    response.json().then(function (data) {
-                        var catActivity = data.activity;
-                        var catAccess = ((data.accessibility) * 100) + '%';
-                        var catCategory = data.type;
-                        var catParticipants = data.participants;
-                        var catPrice = data.price;
-                        var catKey = data.key;
-                        //DisplayResults (catActivity, catAccess, catCategory, catParticipants, catPrice)
-                        //pass activity to search yt videos
-                        searchVideos(catActivity);
-                    })
-                } else {
-                    alert('Error: ' + response.statusText);
-                }
-            })
-            .catch(function (error) {
-                alert('Error connecting to API server');
-            });
-
-    }
+jQuery(function ($) {
+    // Your code using failsafe $ alias here...
 
 
-    //Random search button
-    function randomgenerator() {
-        var randomurl = 'http://www.boredapi.com/api/activity/'
-        fetch(randomurl)
-            .then(function (response) {
-                if (response.ok) {
-                    response.json().then(function (data) {
-                        var Activity = data.activity;
-                        var Access = ((data.accessibility) * 100) + '%';
-                        var Category = data.type;
-                        var Participants = data.participants;
-                        var Price = data.price;
-                        var Key = data.key;
-                        //DisplayResults (catActivity, catAccess, catCategory, catParticipants, catPrice)
-                        //pass activity to search yt videos
-                        searchVideos(Activity);
-                    })
-                } else {
-                    alert('Error: ' + response.statusText);
-                }
-            })
-            .catch(function (error) {
-                alert('Error connecting to API server');
-            });
-    }
-
-    //yt call function
-    var searchVideos = function (keywords) {
-        //youtube call to get search results of activity searched & video ids
-        var apiKey = 'AIzaSyDPqjwbmgS43b7TLONJZcJ8sGbF1tOhepA';
-        var ytUrl = 'https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=4&q=' + keywords + `&type=video&videoEmbeddable=true&key=${apiKey}`
-        fetch(ytUrl)
-            .then(function (response) {
-                if (response.ok) {
-                    response.json().then(function (data) {
-                        //build array of video ids from data to pass on to displayVideo function
-                        var videoIds = [];
-                        data.items.forEach(function (element, index) {
-                            var videoObj = element;
-                            var videoId = videoObj.id.videoId;
-                            videoIds.push(videoId);
-                        })
-                        //pass videoIds arr to display video function
-                        displayVideos(videoIds);
-
-                    })
-                } else {
-                    alert('Error ' + response.statusText)
-                }
-            })
-            .catch(function (error) {
-                alert('Error connecting to API youtube server')
-            });
-    }
-
-    //displayVideo function
-    var displayVideos = function (array) {
-        //each method to loop through each element with class=player and change src
-        $('.player').each(function(index){
-            $(this).attr('src', `https://www.youtube.com/embed/${array[index]}?enablejsapi=1`)
-        });
-
-    }
-
-
-    //Materialize 
-    $('select').formSelect();
-    //Button Listener for random search 
-    $('#random').on('click', randomgenerator);
-    //capture value of selected category from dropdown on click & initiate call
+    //capture value of selected category from dropdown
     $('#categorySearch').on('click', function () {
         // console.log($('.browser-default').val());
         var categoryClicked = $('.browser-default').val();
@@ -105,7 +10,108 @@ $(document).ready(function () {
         categoryCall(categoryClicked);
     })
 
+    var categoryCall = function (category) {
 
+        //make call to gather category endpoint data
+        
+        var categoryUrl = 'https://www.boredapi.com/api/activity?type=' + category
+        fetch(categoryUrl)
+            .then(function (response) {
+                if (response.ok) {
+                    response.json().then(function (data) {
+                        console.log('Search by Type of Activity-----');
+                        console.log(data)
+                        // console.log(data.activity);
+                        var catActivity = data.activity;
+                        console.log(catActivity);
+                        // console.log(((data.accessibility)*100) + '%')
+                        var catAccess = ((data.accessibility)*100) + '%';
+                        console.log(catAccess);
+                        // console.log(data.type)
+                        var catCategory= data.type;
+                        console.log(catCategory);
+                        // console.log(data.participants)
+                        var catParticipants = data.participants;
+                        console.log(catParticipants);
+                        // console.log(data.price)
+                        var catPrice = data.price;
+                        console.log(catPrice);
+                        //DisplayResults (catActivity, catAccess, catCategory, catParticipants, catPrice)
+                        //pass activity to search yt videos ====> searchVideos(catActivity);
+                    })
+                } else {
+                    alert('Error: ' + response.statusText);
+                }
+            })
+            .catch(function (error) {
+                alert('Error connecting to API server');
+            });
+
+    }
+
+
+    //Materialize 
+    $('select').formSelect();
+
+    //Random search button
+    function randomgenerator(){
+        var randomurl = 'http://www.boredapi.com/api/activity/'
+        fetch(randomurl)
+            .then(function (response) {
+                if (response.ok) {
+                    response.json().then(function (data) {
+                        console.log('Random Search');
+                        console.log(data)
+                        // console.log(data.activity);
+                        var catActivity = data.activity;
+                        console.log(catActivity);
+                        // console.log(((data.accessibility)*100) + '%')
+                        var catAccess = ((data.accessibility)*100) + '%';
+                        console.log(catAccess);
+                        // console.log(data.type)
+                        var catCategory= data.type;
+                        console.log(catCategory);
+                        // console.log(data.participants)
+                        var catParticipants = data.participants;
+                        console.log(catParticipants);
+                        // console.log(data.price)
+                        var catPrice = data.price;
+                        console.log(catPrice);
+                        //DisplayResults (catActivity, catAccess, catCategory, catParticipants, catPrice)
+                        //pass activity to search yt videos ====> searchVideos(catActivity);
+                    })
+                } else {
+                    alert('Error: ' + response.statusText);
+                }
+            })
+            .catch(function (error) {
+                alert('Error connecting to API server');
+        });}
+
+ //Button Listener for random search 
+
+$('#random').on('click', randomgenerator)
+
+///// Save li text
+$('#save').on('click',function () {
+    
+var newfav=[]
+$("#actcontainer").find("li").each(function()
+{   var $li=$(this);    
+   newfav.push($li.text()) 
+})
+console.log(newfav) 
+favorites =JSON.parse(localStorage.getItem('favs')) || []
+favorites.push(newfav)
+console.log(favorites) 
+
+localStorage.setItem("favs", JSON.stringify(favorites));
+
+
+})
+  
+  
+     
 
 });
 
