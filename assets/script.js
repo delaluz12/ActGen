ac373d4c989183d630ab85993477b541ff1970cc
Mohamed Ahmed <axmedmohamed7@gmail.com>
2021-07-31 13:20:44 -0500

    display price

--- assets/script.js
+++ assets/script.js
@@ -1,164 +1,212 @@
 $(document).ready(function () {
+  var categoryCall = function (category) {
+    //make call to gather category endpoint data
+    var categoryUrl = "https://www.boredapi.com/api/activity?type=" + category;
+    fetch(categoryUrl)
+      .then(function (response) {
+        if (response.ok) {
+          response.json().then(function (data) {
+            var catActivity = data.activity;
+            var catAccess = data.accessibility;
+            var catCategory = data.type;
+            var catParticipants = data.participants;
+            var catPrice = data.price;
+            var catKey = data.key;
+            DisplayResults(
+              catActivity,
+              catAccess,
+              catCategory,
+              catParticipants,
+              catPrice
+            );
+            //pass activity to search yt videos
+            searchVideos(catActivity);
+          });
+        } else {
+          alert("Error: " + response.statusText);
+        }
+      })
+      .catch(function (error) {
+        alert("Error connecting to API server");
+      });
+  };
+//passing string to cost,Accessibility.
+  function DisplayResults(param1, param2, param3, param4, param5) {
+    var access = "";
+    var costAll = "";
 
-    var categoryCall = function (category) {
-        //make call to gather category endpoint data
-        var categoryUrl = 'https://www.boredapi.com/api/activity?type=' + category
-        fetch(categoryUrl)
-            .then(function (response) {
-                if (response.ok) {
-                    response.json().then(function (data) {
-                        var catActivity = data.activity;
-                        var catAccess = ((data.accessibility) * 100) + '%';
-                        var catCategory = data.type;
-                        var catParticipants = data.participants;
-                        var catPrice = data.price;
-                        var catKey = data.key;
-                        //DisplayResults (catActivity, catAccess, catCategory, catParticipants, catPrice)
-                        //pass activity to search yt videos
-                        searchVideos(catActivity);
-                    })
-                } else {
-                    alert('Error: ' + response.statusText);
-                }
-            })
-            .catch(function (error) {
-                alert('Error connecting to API server');
+    if (param5 <= 0.3) {
+        //easy
+        costAll = "Low";
+      } else if (param5 > 0.3 && param5 < 0.8) {
+        //meduim
+        costAll = "Meduim";
+      } else if(param5 >0.7) {
+        //high
+        costAll = "High";
+  
+      } else {
+        costAll= param5;
+      }
+
+    if (param2 <= 0.3) {
+      //easy
+     access = "Easy";
+    } else if (param2 > 0.3 && param2 < 0.8) {
+      //meduim
+      access = "Meduim";
+    } else if(param2 >0.7) {
+      //hard
+      access = "Hard";
+
+    } else {
+        access= param2;
+    }
+ console.log("accessbility -score ", access);
+    $("#allactivity").text(param1);
+    $("#allaccess").text(access);
+    $("#alltype").text(param3);
+    $("#allparticipants").text(param4);
+    $("#allprice").text(costAll);
+  }
+
+  //Random search button
+  function randomgenerator() {
+    var randomurl = "http://www.boredapi.com/api/activity/";
+    fetch(randomurl)
+      .then(function (response) {
+        if (response.ok) {
+          response.json().then(function (data) {
+            var Activity = data.activity;
+            var Access = data.accessibility * 100 + "%";
+            var Category = data.type;
+            var Participants = data.participants;
+            var Price = data.price;
+            var Key = data.key;
+            DisplayResults(Activity, Access, Category, Participants, Price);
+            //pass activity to search yt videos
+            searchVideos(Activity);
+          });
+        } else {
+          alert("Error: " + response.statusText);
+        }
+      })
+      .catch(function (error) {
+        alert("Error connecting to API server");
+      });
+  }
+  
+
+  //yt call function
+  var searchVideos = function (keywords) {
+    //youtube call to get search results of activity searched & video ids
+    var apiKey = "";
+    var ytUrl =
+      "https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=4&q=" +
+      keywords +
+      `&type=video&videoEmbeddable=true&key=${apiKey}`;
+    fetch(ytUrl)
+      .then(function (response) {
+        if (response.ok) {
+          response.json().then(function (data) {
+            //build array of video ids from data to pass on to displayVideo function
+            var videoIds = [];
+            data.items.forEach(function (element, index) {
+              var videoObj = element;
+              var videoId = videoObj.id.videoId;
+              videoIds.push(videoId);
             });
+            //pass videoIds arr to display video function
+            displayVideos(videoIds);
+          });
+        } else {
+          alert("Error " + response.statusText);
+        }
+      })
+      .catch(function (error) {
+        alert("Error connecting to API youtube server");
+      });
+  };
 
-    }
+  //displayVideo function
+  var displayVideos = function (array) {
+    //each method to loop through each element with class=player and change src
+    $(".player").each(function (index) {
+      $(this).attr(
+        "src",
+        `https://www.youtube.com/embed/${array[index]}?enablejsapi=1`
+      );
+    });
+  };
 
-
-    //Random search button
-    function randomgenerator() {
-        var randomurl = 'http://www.boredapi.com/api/activity/'
-        fetch(randomurl)
-            .then(function (response) {
-                if (response.ok) {
-                    response.json().then(function (data) {
-                        var Activity = data.activity;
-                        var Access = ((data.accessibility) * 100) + '%';
-                        var Category = data.type;
-                        var Participants = data.participants;
-                        var Price = data.price;
-                        var Key = data.key;
-                        //DisplayResults (catActivity, catAccess, catCategory, catParticipants, catPrice)
-                        //pass activity to search yt videos
-                        searchVideos(Activity);
-                    })
-                } else {
-                    alert('Error: ' + response.statusText);
-                }
-            })
-            .catch(function (error) {
-                alert('Error connecting to API server');
-            });
-    }
-
-    //yt call function
-    var searchVideos = function (keywords) {
-        //youtube call to get search results of activity searched & video ids
-        var apiKey = 'AIzaSyDPqjwbmgS43b7TLONJZcJ8sGbF1tOhepA';
-        var ytUrl = 'https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=4&q=' + keywords + `&type=video&videoEmbeddable=true&key=${apiKey}`
-        fetch(ytUrl)
-            .then(function (response) {
-                if (response.ok) {
-                    response.json().then(function (data) {
-                        //build array of video ids from data to pass on to displayVideo function
-                        var videoIds = [];
-                        data.items.forEach(function (element, index) {
-                            var videoObj = element;
-                            var videoId = videoObj.id.videoId;
-                            videoIds.push(videoId);
-                        })
-                        //pass videoIds arr to display video function
-                        displayVideos(videoIds);
-
-                    })
-                } else {
-                    alert('Error ' + response.statusText)
-                }
-            })
-            .catch(function (error) {
-                alert('Error connecting to API youtube server')
-            });
-    }
-
-    //displayVideo function
-    var displayVideos = function (array) {
-        //each method to loop through each element with class=player and change src
-        $('.player').each(function(index){
-            $(this).attr('src', `https://www.youtube.com/embed/${array[index]}?enablejsapi=1`)
-        });
-
-    }
-
-
-    //Materialize 
-    $('select').formSelect();
-    //Button Listener for random search 
-    $('#random').on('click', randomgenerator);
-    //capture value of selected category from dropdown on click & initiate call
-    $('#categorySearch').on('click', function () {
-        // console.log($('.browser-default').val());
-        var categoryClicked = $('.browser-default').val();
-        //pass value to make api call
-        categoryCall(categoryClicked);
-    })
+  //Materialize
+  $("select").formSelect();
+  //Button Listener for random search
+  $("#random").on("click", randomgenerator);
+  //capture value of selected category from dropdown on click & initiate call
+  $("#categorySearch").on("click", function () {
+    // console.log($('.browser-default').val());
+    var categoryClicked = $(".browser-default").val();
+    //pass value to make api call
+    categoryCall(categoryClicked);
+  });
 
   //Save function
-  $('#save').on('click',function () {
-    
-    var newfav=[]
-    $("#actcontainer").find("li").each(function()
-    {   var $li=$(this);    
-       newfav.push($li.text()) 
-    })
-    console.log(newfav) 
-    favorites =JSON.parse(localStorage.getItem('favs')) || []
-    favorites.push(newfav)
-    console.log(favorites) 
-    
+  $("#save").on("click", function () {
+    var newfav = [];
+    $("#actcontainer")
+      .find("li")
+      .each(function () {
+        var $li = $(this);
+        newfav.push($li.text());
+      });
+    console.log(newfav);
+    favorites = JSON.parse(localStorage.getItem("favs")) || [];
+    favorites.push(newfav);
+    console.log(favorites);
+
     localStorage.setItem("favs", JSON.stringify(favorites));
-    
-    
-    })  
+  });
 
-    displayfavs()
-//Render saved activities
-    function displayfavs(favorites){
-        favorites=JSON.parse(localStorage.getItem('favs')) || []
-        cardcont=$('#container')
+  displayfavs();
+  //Render saved activities
+  function displayfavs(favorites) {
+    favorites = JSON.parse(localStorage.getItem("favs")) || [];
+    cardcont = $("#container");
 
-        for (let i = 0; i < favorites.length; i++){
-                
-        var card ='<div class="col s4">'+
-                '<div class="card blue-grey darken-1">'+
-                    '<div class="card-content white-text">'+
-                    '<span class="card-title">Idea For You</span>'+
-                    '<ul id="list">'+
-                        '<li>'+favorites[i][0]+'</li>'+
-                        '<li>'+favorites[i][1]+'</li>'+
-                        '<li>'+favorites[i][2]+'</li>'+
-                        '<li>'+favorites[i][3]+'</li>'+
-                        '<li>'+favorites[i][4]+'</li>'+
-                        
-                    '</ul>'+
-                '</div>'+
-                '</div>'+
-            '</div>';
-            cardcont.append(card)     
-        }
-
-        //Reset Favorites
-        $('#reset').on('click',function () {
-            localStorage.removeItem("favs");
-            location.reload()
-        
-        })
-
+    for (let i = 0; i < favorites.length; i++) {
+      var card =
+        '<div class="col s4">' +
+        '<div class="card blue-grey darken-1">' +
+        '<div class="card-content white-text">' +
+        '<span class="card-title">Idea For You</span>' +
+        '<ul id="list">' +
+        "<li>" +
+        favorites[i][0] +
+        "</li>" +
+        "<li>" +
+        favorites[i][1] +
+        "</li>" +
+        "<li>" +
+        favorites[i][2] +
+        "</li>" +
+        "<li>" +
+        favorites[i][3] +
+        "</li>" +
+        "<li>" +
+        favorites[i][4] +
+        "</li>" +
+        "</ul>" +
+        "</div>" +
+        "</div>" +
+        "</div>";
+      cardcont.append(card);
     }
-    
+
+    //Reset Favorites
+    $("#reset").on("click", function () {
+      localStorage.removeItem("favs");
+      location.reload();
+    });
+  }
 });
-
-
-
